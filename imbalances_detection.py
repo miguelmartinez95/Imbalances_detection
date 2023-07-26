a12ad574bcbca955e6a5c6f934b0267eb1ecdb08
Miguel <miguelmartinez_95@outlook.es>
2023-07-26 13:15:43 +0200

    barplot together with lineplot

--- imbalances_detection.py
+++ imbalances_detection.py
@@ -1,9 +1,9 @@
 import sys
 import urllib.request
 
-url1 = 'https://raw.githubusercontent.com/miguelmartinez95/Imbalances_detection/decompensation_function.py'
-url2 = 'https://raw.githubusercontent.com/miguelmartinez95/Imbalances_detection/functions.py'
-url3 = 'https://raw.githubusercontent.com/miguelmartinez95/Imbalances_detection/imbalances_detection.py'
+url1 = 'https://raw.githubusercontent.com/miguelmartinez95/Imbalances_detection/main/decompensation_function.py'
+url2 = 'https://raw.githubusercontent.com/miguelmartinez95/Imbalances_detection/main/functions.py'
+url3 = 'https://raw.githubusercontent.com/miguelmartinez95/Imbalances_detection/main/imbalances_detection.py'
 
 filename, headers = urllib.request.urlretrieve(url1, filename='E:\Documents\Doctorado\PAPERS\Paper_vivienda_social_Victoria\Paper\Imbalance_detection\decompensation_function.py')
 filename, headers = urllib.request.urlretrieve(url2, filename='E:\Documents\Doctorado\PAPERS\Paper_vivienda_social_Victoria\Paper\Imbalance_detection\functions.py')
@@ -13,8 +13,6 @@ filename, headers = urllib.request.urlretrieve(url3, filename='E:\Documents\Doct
 #Ejemplo de path
 sys.path.insert(1, 'E:\Documents\Doctorado\PAPERS\Paper_vivienda_social_Victoria\Paper\Imbalance_detection')
 from decompesation_function import decompesation_analisis
-
-
 
 #Subir datos
 path=r'E:\Documents\Doctorado\PAPERS\Paper_vivienda_social_Victoria\Paper\Data'
