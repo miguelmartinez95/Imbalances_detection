5be81476e74807b68405b265e5cad9d78803522e
Miguel <miguelmartinez_95@outlook.es>
2023-09-12 16:16:19 +0200

    Cleaning

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # Imbalances_detection
-Scripts and data used for developing the study of imbalances detection through residential buildings.
+Scripts and data used for developing the study of imbalances detection through residential buildings. 
 
 
-Folder to perform imbalance search:
+Folders to perform imbalance search:
 
 - Scripts: 
 
-	- imbalances_detections.py: main
+	- imbalances_detections.py: main script. Selección de periodos para efectuar análisis, número de grupos ([] realizamos búsqueda de 	óptimo) y estructura del edificio
 
-	- functions.py: two specific functions to process the data and perform the detection with plots
+	- functions.py: specific functions to process the data and perform the detection with plots
 
 	- decompesation_function.py: function to create the thermal environments
 	
@@ -16,13 +16,13 @@ Folder to perform imbalance search:
 	Files with data year by year and one agregate
 	
 - Graficos: 
-	Folder to save the generated plots
+	Folder to save the generated plots: barras, grupos y temporales
 	
 
 #IMBALANCE SEARCH
 
-This search is based on 4 decompesation types:
+This search is based on 2 decompesation types:
 
 - Type 1 (sup): High KPI (> p75) and low thermal gap (<p50) plus dwellings with very high KPI without having a high thermal gap (<p75)
 
-- Type 3 (inf): Low KPI (< p25) and high thermal gap (>p50) plus dwellings with very low KPI without having a low thermal gap (>p25)
+- Type 2 (inf): Low KPI (< p25) and high thermal gap (>p50) plus dwellings with very low KPI without having a low thermal gap (>p25)
--- imbalances_detection.py
+++ imbalances_detection.py
@@ -3,18 +3,19 @@ import urllib.request
 import numpy as np
 import pandas as pd
 
+#Specific case for GitHub repository
 url1 = 'https://raw.githubusercontent.com/miguelmartinez95/Imbalances_detection/main/decompensation_function_def.py'
 url2 = 'https://raw.githubusercontent.com/miguelmartinez95/Imbalances_detection/main/functions_def.py'
 
 filename, headers = urllib.request.urlretrieve(url1, filename=r'E:\Documents\Doctorado\PAPERS\Paper_vivienda_social_Victoria\Paper\Imbalance_detection\decompensation_function_def.py')
 filename, headers = urllib.request.urlretrieve(url2, filename=r'E:\Documents\Doctorado\PAPERS\Paper_vivienda_social_Victoria\Paper\Imbalance_detection\functions_def.py')
 
-#Ejemplo de path
+#Ejemplo de path (where the scripts are)
 sys.path.insert(1, 'E:\Documents\Doctorado\PAPERS\Paper_vivienda_social_Victoria\Paper\Imbalance_detection')
 from decompensation_function_def import decompesation_analisis
 from functions_def import data_structure
 
-#Subir datos
+#Localización de datos
 path=r'E:\Documents\Doctorado\PAPERS\Paper_vivienda_social_Victoria\Paper\Data'
 
 #Guardar gráficos
@@ -30,7 +31,7 @@ letras=3
 portales=3
 pisos=8
 
-# Creamos variable que diferencia bloques por pisos (hasta ibamos con los dos bloques)
+# Creamos variable que diferencia bloques por pisos (los dos edificios)
 bloques = np.concatenate([np.repeat('Derechos', 72), np.repeat('Villabuena', 54)])
 bloques = pd.DataFrame(bloques).astype('category')
 
@@ -46,15 +47,13 @@ nombres = t_int.columns
 t_out = t_ext.iloc[:, 4]
 rad = radiation.iloc[:, 4]
 
-#elegimos el edificio concreto
+#Elegimos el edificio concreto
 consumos = consumos.iloc[:, np.where(bloques == edificio)[0]]
-#t_out = t_out.iloc[:, np.where(bloques == edificio)[0]]
 nombres = nombres[np.where(bloques == edificio)[0]]
 t_int = t_int.iloc[:, np.where(bloques == edificio)[0]]
-#rad = rad.iloc[:, np.where(bloques == edificio)[0]]
 
 #Metros cuadrados
-m2 = pd.DataFrame(np.tile(np.array([84, 64, 84]), 24))
-#m2 = pd.DataFrame(np.tile(np.array([84, 64, 84]), 18))
+m2 = pd.DataFrame(np.tile(np.array([84, 64, 84]), 24)) #Derechos
+#m2 = pd.DataFrame(np.tile(np.array([84, 64, 84]), 18)) #Villabuena
 
 decompesation_analisis(path2, consumos, t_int, t_out, rad, m2, grupos,nombres, letras, portales, pisos, year, dates)
