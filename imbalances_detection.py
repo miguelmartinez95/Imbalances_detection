db96bfcdf6baa0d1971b15dcd0e43a53bea66fb9
Miguel <miguelmartinez_95@outlook.es>
2023-09-18 13:21:28 +0200

    plt show

--- imbalances_detection.py
+++ imbalances_detection.py
@@ -26,11 +26,11 @@ start='2021-12-01 00:00:00'
 end='2022-03-28 23:00:00'
 min_horas = 5 #horas mínima de consumos para considerarlos
 grupos = [] #if empty the function search for the optimal number
-edificio = 'Derechos'
+edificio = 'Villabuena'
 agregado=False
 letras=3
 portales=3
-pisos=8  #derechos 8 villabuena 6
+pisos=6  #derechos 8 villabuena 6
 datos_sotanos = False
 
 
