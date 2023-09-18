ee577d2408780c453c7534235abc72bd690c53d6
Miguel <miguelmartinez_95@outlook.es>
2023-09-18 11:48:48 +0200

    plt show

--- functions.py
+++ functions.py
@@ -414,6 +414,7 @@ def detection(edificio, dates, year, var, var_con, diff, o_bool, exterior, rad, 
         plt.show()
 
         opt = np.where(np.diff(Sum_of_squared_distances)/ np.delete(Sum_of_squared_distances, len(Sum_of_squared_distances)-1) <=0.025)[0][0]
+
         print('Optimal number of groups:', K[opt+1])
         raise NameError('Number of groups needed')
 
